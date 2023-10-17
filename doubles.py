89d633387cef8155a57f34258ed13dfea0ec77d8
Lemminkyinen <jarpe2@hotmail.fi>
2023-10-17 09:09:04 +0300

    more tests

--- doubles.py
+++ doubles.py
@@ -2,6 +2,7 @@ import re
 import string
 import random
 import python_rust
+import numpy as np
 
 # Python traditional ZIP version
 def count_doubles(val):
@@ -12,6 +13,15 @@ def count_doubles(val):
     return total
 
 
+def count_once(val):
+    total = 0
+    c1 = val[0]
+    for c2 in val[1:]:
+        if c1 == c2:
+            total += 1
+        c1 = c2
+    return total
+
 # Python REGEXP version
 double_re = re.compile(r'(?=(.)\1)')
 
@@ -20,15 +30,32 @@ def count_doubles_regex(val):
 
 
 
+def count_double_numpy(val):
+    ng=np.fromstring(val,dtype=np.byte)
+    return np.sum(ng[:-1]==ng[1:])
+
+
 # Benchmark it
 # generate 1M of random letters to test it
 val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
 
-def test_pure_python(benchmark):
+def test_python_zip(benchmark):
     benchmark(count_doubles, val)
 
-def test_regex(benchmark):
+def test_python_regex(benchmark):
     benchmark(count_doubles_regex, val)
 
-def test_rust(benchmark):
-    benchmark(python_rust.count_doubles_traditional, val)
+def test_python_once(benchmark):
+    benchmark(count_once, val)
+
+def test_python_numpy(benchmark):
+    benchmark(count_double_numpy, val)
+
+def test_rust_zip(benchmark):
+    benchmark(python_rust.count_doubles_zip, val)
+
+def test_rust_once(benchmark):
+    benchmark(python_rust.count_doubles_once, val)
+
+def test_rust_bytes(benchmark):
+    benchmark(python_rust.count_doubles_bytes, val)
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 #[pyfunction]
-fn count_doubles_traditional(val: &str) -> PyResult<u64> {
+fn count_doubles_zip(val: &str) -> PyResult<u64> {
     let mut total = 0u64;
     // traditional way
     for (c1, c2) in val.chars().zip(val.chars().skip(1)) {
@@ -18,10 +13,46 @@ fn count_doubles_traditional(val: &str) -> PyResult<u64> {
 
     Ok(total)
 }
+
+#[pyfunction]
+fn count_doubles_once(val: &str) -> PyResult<u64> {
+    let mut total = 0u64;
+    let mut chars = val.chars();
+    if let Some(mut c1) = chars.next() {
+        for c2 in chars {
+            if c1 == c2 {
+                total += 1;
+            }
+            c1 = c2;
+        }
+    }
+    Ok(total)
+}
+
+
+#[pyfunction]
+fn count_doubles_bytes(val: &str) -> PyResult<u64> {
+    let mut total = 0u64;
+
+    let mut chars = val.bytes();
+    if let Some(mut c1) = chars.next() {
+        for c2 in chars {
+            if c1 == c2 {
+                total += 1;
+            }
+            c1 = c2;
+        }
+    }
+
+    Ok(total)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn python_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(count_doubles_traditional, m)?)?;
+    m.add_function(wrap_pyfunction!(count_doubles_zip, m)?)?;
+    m.add_function(wrap_pyfunction!(count_doubles_once, m)?)?;
+    m.add_function(wrap_pyfunction!(count_doubles_bytes, m)?)?;
     Ok(())
 }
