dd95bb0f7a2dca97c836529897059340882197cd
Jarmo Tenhunen <jarmo@seravo.com>
2024-02-09 12:07:13 +0200

    Update tests

--- doubles.py
+++ doubles.py
@@ -1,11 +1,14 @@
+import random
 import re
 import string
-import random
-import python_rust
+
 import numpy as np
+import python_rust
+from pytest_benchmark.fixture import BenchmarkFixture
+
 
 # Python traditional ZIP version
-def count_doubles(val):
+def count_doubles(val: str):
     total = 0
     for c1, c2 in zip(val, val[1:]):
         if c1 == c2:
@@ -13,7 +16,7 @@ def count_doubles(val):
     return total
 
 
-def count_once(val):
+def count_once(val: str):
     total = 0
     c1 = val[0]
     for c2 in val[1:]:
@@ -22,40 +25,49 @@ def count_once(val):
         c1 = c2
     return total
 
-# Python REGEXP version
-double_re = re.compile(r'(?=(.)\1)')
 
-def count_doubles_regex(val):
+def count_doubles_regex(val: str):
+    double_re = re.compile(r"(?=(.)\1)")
     return len(double_re.findall(val))
 
 
-
-def count_double_numpy(val):
-    ng=np.fromstring(val,dtype=np.byte)
-    return np.sum(ng[:-1]==ng[1:])
+def count_double_numpy(val: str):
+    ng = np.fromstring(val, dtype=np.byte)
+    return np.sum(ng[:-1] == ng[1:])
 
 
 # Benchmark it
-# generate 1M of random letters to test it
-val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
+# generate 50M of random letters to test it
+val = "".join(random.choice(string.ascii_letters) for i in range(50_000_000))
 
-def test_python_zip(benchmark):
+
+def test_python_zip(benchmark: BenchmarkFixture):
     benchmark(count_doubles, val)
 
-def test_python_regex(benchmark):
+
+def test_python_regex(benchmark: BenchmarkFixture):
     benchmark(count_doubles_regex, val)
 
-def test_python_once(benchmark):
+
+def test_python_once(benchmark: BenchmarkFixture):
     benchmark(count_once, val)
 
-def test_python_numpy(benchmark):
+
+def test_python_numpy(benchmark: BenchmarkFixture):
     benchmark(count_double_numpy, val)
 
-def test_rust_zip(benchmark):
+
+def test_rust_zip(benchmark: BenchmarkFixture):
     benchmark(python_rust.count_doubles_zip, val)
 
-def test_rust_once(benchmark):
+
+def test_rust_once(benchmark: BenchmarkFixture):
     benchmark(python_rust.count_doubles_once, val)
 
-def test_rust_bytes(benchmark):
+
+def test_rust_bytes(benchmark: BenchmarkFixture):
     benchmark(python_rust.count_doubles_bytes, val)
+
+
+def test_rust_bytes_unsafe(benchmark: BenchmarkFixture):
+    benchmark(python_rust.count_doubles_unsafe, val)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use pyo3::prelude::*;
 
-
 #[pyfunction]
 fn count_doubles_zip(val: &str) -> PyResult<u64> {
     let mut total = 0u64;
@@ -10,7 +9,6 @@ fn count_doubles_zip(val: &str) -> PyResult<u64> {
             total += 1;
         }
     }
-
     Ok(total)
 }
 
@@ -29,7 +27,6 @@ fn count_doubles_once(val: &str) -> PyResult<u64> {
     Ok(total)
 }
 
-
 #[pyfunction]
 fn count_doubles_bytes(val: &str) -> PyResult<u64> {
     let mut total = 0u64;
@@ -43,16 +40,30 @@ fn count_doubles_bytes(val: &str) -> PyResult<u64> {
             c1 = c2;
         }
     }
+    Ok(total)
+}
 
+#[pyfunction]
+unsafe fn count_doubles_unsafe(val: &str) -> PyResult<u64> {
+    let mut total = 0u64;
+    let bytes = val.as_bytes();
+    let len = bytes.len();
+
+    let ptr = bytes.as_ptr();
+    for i in 0..len-1 {
+        if *ptr.add(i) == *ptr.add(i + 1) {
+            total += 1;
+        }
+    }
     Ok(total)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn python_rust(_py: Python, m: &PyModule) -> PyResult<()> {
-    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(count_doubles_zip, m)?)?;
     m.add_function(wrap_pyfunction!(count_doubles_once, m)?)?;
     m.add_function(wrap_pyfunction!(count_doubles_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(count_doubles_unsafe, m)?)?;
     Ok(())
 }
